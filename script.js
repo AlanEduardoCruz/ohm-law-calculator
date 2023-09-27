7f9401d601a4d27bfa92754dc6bc28296e9aaed6
AlanEduardoCruz <alan-hd@hotmail.com>
2023-09-27 08:42:18 -0300

    alteração no codigo Js

--- script.js
+++ script.js
@@ -8,76 +8,70 @@ const selectValue = document.getElementById("select-value");
 
 // função criada para realizar os calculos
 function calculateValue() {
-  const fisrtInputValue = document.querySelector(".first-input-value").value;
-  const secondInputValue = document.querySelector(".second-input-value").value;
+  const firstValue = parseFloat(fisrtInputValue.value);
+  const secondValue = parseFloat(secondInputValue.value);
 
-  // Realiza os calculos
-  const calculatedVoltage = fisrtInputValue * secondInputValue;
-  const calculatedCurrent = fisrtInputValue / secondInputValue;
-  const calculatedResistance = fisrtInputValue / secondInputValue;
-  const calculatedPower = fisrtInputValue * secondInputValue;
-
-  // Se o valor do select for verdadeiro ele imprime o valor calculado
-  if (selectValue.value == "tensao") {
-    //Se a opção for "tensão" imprima na tela o seguinte valor
-    console.log((resultValue.value = calculatedVoltage + "V"));
-  }
-  if (selectValue.value == "corrente") {
-    //Se a opção for "corrente" imprima na tela o seguinte valor
-    console.log((resultValue.value = calculatedCurrent + "A"));
-  }
-  if (selectValue.value == "resistencia") {
-    //Se a opção for "resistencia" imprima na tela o seguinte valor
-    console.log((resultValue.value = calculatedResistance + "Ω"));
-  }
-  if (selectValue.value == "potencia") {
-    //Se a opção for "potencia" imprima na tela o seguinte valor
-    console.log((resultValue.value = calculatedPower + "W"));
+  // Verifica se os valores são válidos
+  if (isNaN(firstValue) || isNaN(secondValue)) {
+    resultValue.textContent = "Por favor, insira valores numéricos validos.";
+  } else {
+    // realizando os cálculos
+    let result;
+    if (selectValue.value == "tensao") {
+      result = firstValue * secondValue;
+      resultValue.textContent = `O resultado é: ${result}V`;
+      console.log(result);
+    }
+    if (selectValue.value == "corrente") {
+      result = firstValue / secondValue;
+      resultValue.textContent = `O resultado é: ${result}A`;
+    }
+    if (selectValue.value == "resistencia") {
+      result = firstValue / secondValue;
+      resultValue.textContent = `O resultado é: ${result}Ω`;
+    }
+    if (selectValue.value == "potencia") {
+      result = firstValue * secondValue;
+      resultValue.textContent = `O resultado é: ${result}W`;
+    }
   }
 }
-
 //Função criada para mapearos elementos do HTML
-
 function changeName() {
   //Mapeando elementos do HTML
   const currentName = document.getElementById("current-name");
   const resistanceName = document.getElementById("resistance-name");
   const resultName = document.getElementById("result-name");
 
+  //Definindo nomes com base na seleção
   if (selectValue.value == "corrente") {
-    //Se a opção for "corrende (I)" coloque os seguintes valores de entrada
     currentName.innerHTML = "Tensao (V)";
     resistanceName.innerHTML = "Resistência (Ω)";
     resultName.innerHTML = "Corrente (I)";
-    
   }
 
   if (selectValue.value == "resistencia") {
-    //Se a opção for "resistencia (Ω)"  coloqe os seguintes valores de entrada
     resistanceName.innerHTML = " Corrente (I)";
     currentName.innerHTML = "Tensão (V)";
     resultName.innerHTML = "Resistência (Ω)";
   }
 
   if (selectValue.value == "potencia") {
-    // Se a opção for "potência (W)" coloque os seguintes valores de entrada
     currentName.innerHTML = "Tensão (V)";
     resistanceName.innerHTML = "Corrente (I)";
     resultName.innerHTML = "Potência (W)";
   }
   if (selectValue.value == "tensao") {
-    // Se a opção for "Tensão", redefina os valores dos campos de entrada
     currentName.innerHTML = "Corrente (I)";
     resistanceName.innerHTML = "Resistência (Ω)";
     resultName.innerHTML = "Tensão (V)";
   }
 }
-
-// Realiza o reset para as configurações iniciais
+// Função criada para redefinir os valores para as configurações iniciais
 function resetValues() {
   fisrtInputValue.value = "";
   secondInputValue.value = "";
-  resultValue.value = "";
+  resultValue.textContent = "Resultado";
 
   // Redefinir os nomes para os valores originais
   const currentName = document.getElementById("current-name");
@@ -97,7 +91,6 @@ originalResistanceName = document.getElementById("resistance-name").innerHTML;
 orinalResultName = document.getElementById("result-name").innerHTML;
 
 //chama as funções abaixo a partir de cada evento
-
 selectValue.addEventListener("change", changeName);
 buttonCalculate.addEventListener("click", calculateValue);
 buttonReset.addEventListener("click", resetValues);
